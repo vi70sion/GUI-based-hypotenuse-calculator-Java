f88b7e3744bf71f3462f14c3551b745e9b2c2e6b
Evaldas Lupeikis <levaldas@hotmail.com>
2024-03-09 20:58:50 +0200

    Added checking for invalid characters.

--- GUI-Triangle-Hypotenuse-Calculator/src/Main.java
+++ GUI-Triangle-Hypotenuse-Calculator/src/Main.java
@@ -7,13 +7,14 @@ public class Main {
 		
 		double x, y, z;
 		Scanner scanner = new Scanner(System.in);
-		
-		x = Double.parseDouble(JOptionPane.showInputDialog("Enter triangle side x"));
-		y = Double.parseDouble(JOptionPane.showInputDialog("Enter triangle side y"));
-		
-		z = Math.sqrt((x*x)+(y*y));
-		
-		JOptionPane.showMessageDialog(null, "Triangle hypotenuse is "+z);
+		try {
+			x = Double.parseDouble(JOptionPane.showInputDialog("Enter triangle side x"));
+			y = Double.parseDouble(JOptionPane.showInputDialog("Enter triangle side y"));
+			z = Math.sqrt((x*x)+(y*y));
+			JOptionPane.showMessageDialog(null, "Triangle hypotenuse is "+z);
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Incorrect input");
+		} 
 		scanner.close();
 
 	}
