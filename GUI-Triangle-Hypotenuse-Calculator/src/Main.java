1b4eb5209e4a2f754a131b9a736e44954237cb24
Evaldas Lupeikis <levaldas@hotmail.com>
2024-03-10 23:25:19 +0200

    Added window title, message types.

--- GUI-Triangle-Hypotenuse-Calculator/src/Main.java
+++ GUI-Triangle-Hypotenuse-Calculator/src/Main.java
@@ -9,12 +9,12 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		try {
-			x = Double.parseDouble(JOptionPane.showInputDialog("Enter triangle side x"));
-			y = Double.parseDouble(JOptionPane.showInputDialog("Enter triangle side y"));
+			x = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter triangle side x", "Triangle hypotenuse", JOptionPane.QUESTION_MESSAGE));
+			y = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter triangle side y", "Triangle hypotenuse", JOptionPane.QUESTION_MESSAGE));
 			z = Math.sqrt((x*x)+(y*y));
-			JOptionPane.showMessageDialog(null, "Triangle hypotenuse is "+z);
+			JOptionPane.showMessageDialog(null, "Triangle hypotenuse is "+z, "Triangle hypotenuse", JOptionPane.PLAIN_MESSAGE);
 		} catch (NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "Incorrect input");
+			JOptionPane.showMessageDialog(null, "Incorrect input", "Triangle hypotenuse", JOptionPane.ERROR_MESSAGE);
 		} 
 		scanner.close();
 
